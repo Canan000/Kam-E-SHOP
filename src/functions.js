ef6e16a9c8ec19c9b8374a9ae7f51d87a5e0c929
nacim <linnnux@hotmail.fr>
2023-02-18 15:58:53 +0000

    structure review

---
+++ src/functions.js
@@ -0,0 +1,7 @@
+// https://github.com/users/linnnux/projects/16/views/1?layout=board&pane=issue&itemId=20837820
+function helloMessage()
+{
+    //write code here
+    //@params id = 'helloMessage'
+
+}
