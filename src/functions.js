431fedf27bb4d19a85f244039290d1d6d6064215
nacim <linnnux@hotmail.fr>
2023-02-19 19:52:29 +0000

    add main.js, displayMainTitle & displayCategorContent functions

--- src/functions.js
+++ src/functions.js
@@ -3,5 +3,22 @@ function helloMessage()
 {
     //write code here
     //@params id = 'helloMessage'
+    console.log('execute helloMessage');
+}
 
+function displayMainTitle()
+{
+  //display the head title on id 'headMainTitle'
+  //display the main title on id 'mainTitle'
+
+  document.getElementById('mainTitle').innerHTML = 'Kam -eShop';
+  console.log('execute displayMainTitle');
+}
+
+function displayCategoryContent(categoryId, categoryContent)
+{
+
+  // @params = #id = maybe category1, category2 or category3
+  document.getElementById(categoryId).innerHTML = categoryContent;
+  console.log('execute displayCategoryContent');
 }
---
+++ src/main.js
@@ -0,0 +1,22 @@
+helloMessage();
+
+displayMainTitle();
+
+
+// this update content catgegories
+
+var categoryContent = '<h2>Femmes</h2> <p> hello this is content of category 1';
+
+displayCategoryContent('category1', categoryContent);
+
+categoryContent = '<h2>Hommes</h2> <p> hello this is content of category 2';
+
+displayCategoryContent('category2', categoryContent);
+
+categoryContent = '<h2>Enfants</h2> <p> hello this is content of category 3';
+
+displayCategoryContent('category3', categoryContent);
+
+// this update head title contant
+
+displayCategoryContent('headMainTitle', 'Kam e-Shop');
